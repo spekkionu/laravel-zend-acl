6dd70f3ca98dc2396b7895625aa012f8bb50c44a
Jonathan Bernardi <jonathan@digitalcanvas.com>
2013-10-02 12:38:41 -0700

    Initial Commit

---
+++ src/Spekkionu/ZendAcl/ZendAclServiceProvider.php
@@ -0,0 +1,50 @@
+<?php namespace Spekkionu\ZendAcl;
+
+use Illuminate\Support\ServiceProvider;
+use Zend\Permissions\Acl\Acl;
+use Zend\Permissions\Acl\Role\GenericRole as Role;
+use Zend\Permissions\Acl\Resource\GenericResource as Resource;
+
+class ZendAclServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('spekkionu/laravel-zend-acl');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->app['acl'] = $this->app->share(function($app)
+    {
+    	return new Acl;
+    });
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array('acl');
+	}
+
+}
